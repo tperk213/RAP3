71ffd81038975b5b081f4e02f96f2a9e63416277
GemjuSherpa <gemju.s1@gmail.com>
2021-09-29 21:45:13 +1000

    Partial details view data populated

---
+++ RAP3/Publication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAP3
+{
+    class Publication
+    {
+        public String Title { get; set; }
+        public String Doi { get; set; }
+    }
+}
