f9e6dd470423c56b4a55feaee7a0bc906b339972
GemjuSherpa <gemju.s1@gmail.com>
2021-10-15 00:31:13 +1100

    Add project files.

---
+++ RAP3/DatabaseAdapter.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace RAP3
+{
+    abstract class DatabaseAdapter
+    {
+        private static string server = "alacritas.cis.utas.edu.au";
+        private static string db = "kit206";
+        private static string user = "kit206";
+        private static string password = "kit206";
+        public static MySqlConnection conn;
+
+        public static Dictionary<char, EmploymentLevel> CharToEmploymentLevel = new Dictionary<char, EmploymentLevel>
+            {
+                { 'A', EmploymentLevel.Postdoc },
+                { 'B', EmploymentLevel.Lecturer },
+                { 'C', EmploymentLevel.SeniorLecturer },
+                { 'D', EmploymentLevel.AssociateProfesor },
+                { 'E', EmploymentLevel.Profesor },
+            };
+
+        public static void GetConnection()
+        {
+            if (conn == null)
+            {
+                string connectionString = String.Format("Database={0}; Data Source={1}; User Id ={2}; Password={3}",
+                    db, server, user, password);
+                conn = new MySqlConnection(connectionString);
+            }
+        }
+        /* public DatabaseHandler()
+         {
+             server = "alacritas.cis.utas.edu.au";
+             database = "kit206";
+             uid = "kit206";
+             password = "kit206";
+
+             Console.WriteLine("Connecting to Database ....");
+             string connectionString = String.Format("Database={0}; Data Source={1}; User Id ={2}; Password={3}; SSL Mode=0",
+                 database, server, uid, password);
+             try
+             {
+                 conn = new MySqlConnection(connectionString);
+             }
+             catch (Exception e)
+             {
+                 Console.WriteLine("Exception caught {0}", e);
+             }
+             Console.WriteLine("Database connection established ....");
+
+         }*/
+
+        public static MySqlDataReader RunCommand(String command)
+        {
+            GetConnection();
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Couldnt open connection to database {0}", e);
+            }
+            MySqlCommand cmd = new MySqlCommand(command, conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            return rdr;
+        }
+        public static List<Researcher> LoadResearchers()
+        {
+            Console.WriteLine("Loading Researchers ....");
+            string cmd = "select `id`, `given_name`, `family_name`, `email`, `title`, `level` from researcher;";
+            MySqlDataReader rdr = RunCommand(cmd);
+
+            var researcherList = new List<Researcher>();
+            Researcher res;
+
+            while (rdr.Read())
+            {
+                res = new Researcher();
+                res.Id = rdr.GetInt32("id");
+                res.FirstName = rdr.GetString("given_name");
+                res.LastName = rdr.GetString("family_name");
+                res.Email = rdr.GetString("email");
+                res.Title = rdr.GetString("title");
+                res.setFormalName();
+
+                EmploymentLevel level;
+                if (rdr.IsDBNull(rdr.GetOrdinal("level")))
+                {
+                    level = EmploymentLevel.Student;
+                }
+                else
+                {
+                    level = CharToEmploymentLevel[rdr.GetChar("level")];
+                }
+                res.Level = level;
+
+                researcherList.Add(res);
+            }
+
+            conn.Close();
+            return researcherList;
+        }
+
+
+        //Fetch full researcher details
+        //
+        public static Researcher FetchFullResearcherDetails(Researcher researcher) {
+
+            string cmd = String.Format("select * from researcher where `id` = {0};", researcher.Id);
+            MySqlDataReader rdr = RunCommand(cmd);
+
+            if (rdr.Read())
+            {
+                researcher.FirstName = rdr.GetString("given_name");
+                researcher.LastName = rdr.GetString("family_name");
+                researcher.Title = rdr.GetString("title");
+                researcher.Campus = rdr.GetString("campus");
+                researcher.Email = rdr.GetString("email");
+                researcher.CommencedCurrentPosition = rdr.GetString("current_start");
+                researcher.CommencedInstitution = rdr.GetString("utas_start");
+                researcher.PhotoUrl = rdr.GetString("photo");
+                researcher.Unit = rdr.GetString("unit");
+                //researcher.Degree = rdr.GetString("degree");
+            }
+            rdr.Close();
+
+            researcher.Publications = DatabaseAdapter.LoadPublications(researcher);
+            researcher.Positions = DatabaseAdapter.GetPositionHistory(researcher);
+
+            return researcher;
+        }
+
+        public static Publication FetchFullPublicationDetails(Publication publication)
+        {
+            string cmd = String.Format("select * from publication");
+            MySqlDataReader rdr = RunCommand(cmd);
+
+            if (rdr.Read())
+            {
+
+            }
+            rdr.Close();
+
+            return publication;
+        }
+
+        public static ObservableCollection<Publication> LoadPublications(Researcher r)
+        {
+            ObservableCollection<Publication> publications = new ObservableCollection<Publication>();
+            string cmd = String.Format("select `title`, `doi`, `authors`, `year`, `type`, `cite_as`,`available` from `publication` where `doi` in (select `doi` from `researcher_publication` where `researcher_id` = {0});", r.Id);
+            MySqlDataReader rdr = RunCommand(cmd);
+            while (rdr.Read())
+            {
+                Publication p = new Publication();
+                p.Doi = rdr.GetString("doi");
+                p.Title = rdr.GetString("title");
+                p.Authors = rdr.GetString("authors");
+                p.CiteAs = rdr.GetString("cite_as");
+                p.Available = rdr.GetDateTime("available");
+                p.PublicationYear = rdr.GetInt32("year");
+
+                //Created ToEnum function for this in Class Enum.cs
+                p.Type = rdr.GetString("type").ToEnum<OutputType>();
+
+                publications.Add(p);
+            }
+            conn.Close();
+            return publications;
+
+        }
+
+        //get position details
+        public static ObservableCollection<Position> GetPositionHistory(Researcher r)
+        {
+            ObservableCollection<Position> position = new ObservableCollection<Position>();
+            string cmd = String.Format("select `start`,`end` from position where `id` = {0};", r.Id);
+            MySqlDataReader rdr = RunCommand(cmd);
+            while (rdr.Read())
+            {
+                Position p = new Position();
+                p.start = rdr.GetString("start");
+                //p.end = rdr.GetString("end");
+                position.Add(p);
+            }
+            conn.Close();
+            return position;
+
+        }
+
+
+    }
+}
---
+++ RAP3/ResearcherController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace RAP3
+{
+    class ResearcherController
+    {
+        //DatabaseAdapter referece
+        private List<Researcher> masterResearcherList;
+        private ObservableCollection<Researcher> viewableResearcherList;
+        private SearchTree tree;
+
+        public Researcher CurrentlySelected { get; private set; }
+
+        public ResearcherController()
+        {
+            viewableResearcherList = new ObservableCollection<Researcher>();
+            List<Researcher> researchers = DatabaseAdapter.LoadResearchers();
+            masterResearcherList = researchers;
+            tree = new SearchTree();
+
+            foreach (var curRes in masterResearcherList)
+            {
+                viewableResearcherList.Add(curRes);
+                tree.InsertIntoTree(curRes);
+            }
+            
+
+        }
+
+        
+
+        public ObservableCollection<Researcher> GetResearchers()
+        {
+            return viewableResearcherList;
+        }
+
+        public void FilterByLevel(EmploymentLevel requestedLevel)
+        {
+            viewableResearcherList.Clear();
+            tree.Clear();
+            if( requestedLevel != EmploymentLevel.All)
+            {
+                var filtered = from researcher in masterResearcherList where researcher.Level == requestedLevel select researcher;
+
+                filtered.ToList().ForEach(viewableResearcherList.Add);
+                // Filter the reasearchers by the level indicated in the dropdown box should be an event handler
+            }
+            else
+            {
+                masterResearcherList.ForEach(viewableResearcherList.Add);
+            }
+
+            viewableResearcherList.ToList().ForEach(tree.InsertIntoTree);
+
+        }
+
+        public void FilterByName(String name)
+        {
+            viewableResearcherList.Clear();
+            var filtered = tree.Search(name);
+            filtered.ToList().ForEach(viewableResearcherList.Add);
+        }
+
+
+        //Get all the details of selected researcher
+        public void SelectItem(Researcher researcher)
+        {
+            CurrentlySelected = researcher;
+
+            DatabaseAdapter.FetchFullResearcherDetails(CurrentlySelected);
+        }
+
+    }
+}
